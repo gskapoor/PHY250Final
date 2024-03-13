d99f051707c265ec37fe2fe80092205ba923758c
OfficialDSplayer <jovin.louie@gmail.com>
2024-03-13 13:54:57 -0700

    Update old_main.cpp

--- old_main.cpp
+++ old_main.cpp
@@ -1,6 +1,10 @@
 #define CL_TARGET_OPENCL_VERSION 120
 
-#include <CL/opencl.h>
+#ifdef __APPLE__
+#include <OpenCL/opencl.h>
+#else
+#include <CL/cl.h>
+#endif
 #include <iostream>
 #include <vector>
 
